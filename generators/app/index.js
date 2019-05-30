912cab56fd38768d49c50ca4cb9435e8c00547e7
Ramzi Maalej <ramzi@veamly.com>
2019-05-30 13:26:28 -0700

    fix spock build

--- generators/app/index.js
+++ generators/app/index.js
@@ -101,7 +101,6 @@ module.exports = class extends Generator {
             this._writeJunitTestFiles();
         } else {
             this._writeSpockTestFiles();
-            this._writeJunitTestFiles();
         }
     }
 
